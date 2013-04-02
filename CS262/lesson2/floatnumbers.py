2042bcb7b5c60140bfcb228bb4b9bc9fba111849
Mariz Melo <mm@xchema.com>
2013-04-02 12:21:47 -0700

    float number parser

--- CS262/lesson2/floatnumbers.py
+++ CS262/lesson2/floatnumbers.py
@@ -1,24 +1,25 @@
 import ply.lex as lex # import lex analyzer lib
 
 tokens = (
-  'IDENTIFIER', # valid identifiers 
+  'NUMBER', # valid numbers 
   )
 
 t_ignore = ' ' # shortcut for whitespace
 
 def t_error(token):
-    print("Illegal character '%s'" % token.value[0])
+    print("Illegal number '%s'" % token.value[0])
     token.lexer.skip(1)
 
 
-def t_IDENTIFIER(token):
-  r'^[a-z A-Z][a-z A-Z_0-9.]*'
+def t_NUMBER(token):
+  r'-?[0-9]+(:?\.[0-9]*)?'
+  token.value = float(token.value)
   return token
 
-identifier = "factorial"  # invalid: _factorial , 123factorial
+numbers = "-4.6"  # invalid: 3.4.5.5 a23
 
 identifierlex = lex.lex()
-identifierlex.input(identifier)
+identifierlex.input(numbers)
 
 
 while True:
